84662c16c64574fab5bc890999fa20aa549dfa42
akpi816218 <111009970+akpi816218@users.noreply.github.com>
2022-09-19 10:46:19 -0700

    Add verbosity

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+printf "cURLing the executable...\n"
 curl -s https://raw.githubusercontent.com/akpi816218/feces/gitmaster/feces -O
+printf "Setting executable file permissions (755)...\n"
 chmod 755 feces
 printf "Done installing.\n"
 exit 0
