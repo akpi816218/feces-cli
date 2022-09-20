4894e6e70f364276762e0563def4df6a0f60fb99
akpi816218 <111009970+akpi816218@users.noreply.github.com>
2022-09-20 08:04:27 -0700

    Add composting

--- install.sh
+++ install.sh
@@ -3,5 +3,7 @@ printf "cURLing the executable...\n"
 curl -s https://raw.githubusercontent.com/akpi816218/feces/gitmaster/feces -O
 printf "Setting executable file permissions (755)...\n"
 chmod 755 feces
+printf "Initializing environment...\n"
+mkdir ~/.feces && mkdir ~/.feces/files && mkdir ~/.feces/info
 printf "Done installing.\n"
 exit 0
