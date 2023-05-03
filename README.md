ea3a5adf2b66393af62247bca2c01a0ce91da13b
akpi816218 <816218@seq.org>
2023-05-02 19:22:03 -0700

    save

--- README.md
+++ README.md
@@ -6,8 +6,8 @@ A creatively named trash file manager built for the command line
 
 ## Installation
 
-Download and run [`install.sh`](./install.sh) (or copy-paste the command below):
+Install the npm package (`feces`):
 
 ```bash
-curl -s https://raw.githubusercontent.com/akpi816218/feces/gitmaster/install.sh | bash
+npm i -g feces
 ```
---
+++ bin/feces.js
@@ -0,0 +1,56 @@
+#!/usr/bin/env node
+import { homedir } from 'os';
+import { join } from 'path';
+import { log } from 'console';
+import { access, constants, mkdir, writeFile } from 'fs/promises';
+import mv from 'mv';
+import { argv, cwd } from 'process';
+import Jsoning from 'jsoning';
+argv.shift();
+argv.shift();
+const commandHandlers = {
+    init: async () => {
+        const home = homedir(), fecespath = join(home, '.feces');
+        log('Initializing the feces environment...');
+        mkdir(fecespath);
+        mkdir(join(fecespath, 'files'));
+        writeFile(join(fecespath, 'info'), '{}');
+        log('Initialization complete.');
+    },
+    plop: async () => {
+        if (argv.length !== 2) {
+            process.exitCode = 1;
+            log('Invalid number of arguments.');
+            return;
+        }
+        const ts = Date.now(), name = `${ts}-${argv[1]}`, filepath = join(cwd(), argv[1]), db = new Jsoning(join(homedir(), '.feces', 'index.json')), newpath = join(homedir(), '.feces', 'files', name);
+        try {
+            await access(argv[1], constants.F_OK | constants.R_OK | constants.W_OK);
+        }
+        catch (err) {
+            log('File does not exist or access is denied.');
+            process.exitCode = 1;
+            return;
+        }
+        mv(argv[1], newpath, (err) => { });
+        await db.set(name, {
+            originalPath: filepath,
+            newPath: newpath,
+            timestamp: ts
+        });
+    }
+};
+function main() {
+    if (argv.length === 0) {
+        log('No command specified. Use feces --help to see available commands.');
+        process.exitCode = 1;
+        return;
+    }
+    const cmnd = argv[0];
+    if (commandHandlers[cmnd] === undefined) {
+        log(`Command '${cmnd}' not found. Use feces --help to see available commands.`);
+    }
+    else
+        commandHandlers[cmnd]();
+}
+main();
